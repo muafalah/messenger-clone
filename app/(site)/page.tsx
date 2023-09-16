a703554476d46e9b0a7929cc297bcf33baec43cb
muafalah <muh.ifa.amrillah@gmail.com>
2023-09-16 14:27:50 +0700

    feat: add Auth UI

---
+++ app/(site)/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      {/* Logo */}
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          src="/images/logo.png"
+          className="mx-auto w-auto"
+          height={48}
+          width={48}
+          alt="Logo"
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      {/* Auth Form */}
+      <AuthForm />
+    </div>
+  );
+}
