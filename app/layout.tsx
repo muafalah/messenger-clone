ec98cb7bdbc7924c0ede5a0b9a30616a4e6c6269
muafalah <muh.ifa.amrillah@gmail.com>
2023-09-19 10:47:07 +0700

    feat: add auth context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import AuthContext from "@/Context/AuthContext";
 import ToasterContext from "@/Context/ToasterContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
